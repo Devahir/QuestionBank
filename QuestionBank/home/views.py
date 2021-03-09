b58fb23db7b2df56f6fd52c057d02839d473d4ae
Devahir <64650920+Devahir@users.noreply.github.com>
2021-03-09 08:16:58 +0530

    I have created home App
    In home app I created file urls.py and a function getQRandom in views.py of home app.
    
    the function give response in in json formate.

--- QuestionBank/home/views.py
+++ QuestionBank/home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+
+def giveRandomQ(request):
+    return JsonResponse({'Question':'solutions'})
+
