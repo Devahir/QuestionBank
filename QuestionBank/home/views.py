0948128b5f2bbfa0f25741b02ac54e7e76f8d85c
Devahir <64650920+Devahir@users.noreply.github.com>
2021-03-14 13:16:46 +0530

    new questions have add

--- QuestionBank/home/views.py
+++ QuestionBank/home/views.py
@@ -1,8 +1,30 @@
+import csv
+
+import random
+import os
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
 
+from home.models import Question
+
+# with open('Untitled form.csv') as csvfile:
+#     csvReader=csv.DictReader(csvfile)
 # Create your views here.
 
 def giveRandomQ(request):
+
+    
+
+    # for csvRow in csvReader:
+
+    # Ques=Question(Class=csvRow['Class'],subject=csvRow['subject'],chapter_name=csvRow['chapter'],Lavel=csvRow['Lavel'],disciption=csvRow['discription'],medium=csvRow['medium'],topic_Key=csvRow['topic_Key'],QuestionText=csvRow['QuestionText'])
+    # Ques.save()
+
+    random_num=random.randint(1,12)
+
+    Ques=Question(Class=str(random_num),subject="En",chapter_name="grrg",Lavel="rgg",disciption="gorl",medium="wegnw",topic_Key="kjkw",QuestionText="nejfejnfenj")
+    Ques.save()
+
+
     return JsonResponse({'Question':'solutions'})
 
