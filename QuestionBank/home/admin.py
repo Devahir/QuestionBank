deb02c51c7ec3af446e228108417a7df5e2ce7a0
Devahir <64650920+Devahir@users.noreply.github.com>
2021-03-14 08:45:21 +0530

    created Student, Question Model
    In home app I created Question  and student model.

--- QuestionBank/home/admin.py
+++ QuestionBank/home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Student,Question
 
 # Register your models here.
+
+class ques(admin.ModelAdmin):
+    list_display=["first_name","Stud_class"]
+
+
+admin.site.register(Student,ques)
+
+class qq(admin.ModelAdmin):
+    list_display=["chapter_name","topic_Key"]
+
+admin.site.register(Question,qq)
--- QuestionBank/home/models.py
+++ QuestionBank/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    Class=models.CharField(max_length=3)
+    subject=models.CharField(max_length=4)
+    chapter_name=models.CharField(max_length=40)
+    Lavel=models.CharField(max_length=4)
+    disciption=models.CharField(max_length=50)
+    medium=models.CharField(max_length=3)
+    topic_Key=models.CharField(max_length=40)
+    QuestionText=models.CharField()
+
+class Student(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    Stud_class=models.CharField(max_length=4)
+    medium=models.CharField(max_length=3)
+    course=models.CharField(max_length=4)
+    coin=models.CharField(max_length=4)
+    weight=models.CharField(max_length=4)
