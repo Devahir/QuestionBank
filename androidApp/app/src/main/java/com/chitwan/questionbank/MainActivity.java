f2bbf0315c1d5280602b5985c6f47d3e62d4b3b2
Devahir <64650920+Devahir@users.noreply.github.com>
2021-03-24 14:38:58 +0530

    user Expirience Inhence

--- androidApp/app/src/main/java/com/chitwan/questionbank/MainActivity.java
+++ androidApp/app/src/main/java/com/chitwan/questionbank/MainActivity.java
@@ -1,11 +1,14 @@
 package com.chitwan.questionbank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,7 +29,29 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RequestQueue mQueue;
     TextView textView;
+    String work;
+    String[] mcq;
     ListView option;
+
+    Button button;
+
+    String Answer,Explainaton;
+
+    ConstraintLayout ll;
+
+    LinearLayout line1,line2,line3, line4;
+
+    TextView opA,opB,opC,opD;
+
+    TextView Explain;
+
+    @Override
+    public void onBackPressed() {
+        line1.setVisibility(View.VISIBLE);
+        line2.setVisibility(View.VISIBLE);
+        line3.setVisibility(View.VISIBLE);
+        line4.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +60,26 @@ public class MainActivity extends AppCompatActivity {
 
         mQueue= Volley.newRequestQueue(this);
 
+        opA=findViewById(R.id.opA);
+        opB=findViewById(R.id.opB);
+        opC=findViewById(R.id.opC);
+        opD=findViewById(R.id.opD);
+
+        line1=findViewById(R.id.line1);
+        line2=findViewById(R.id.line2);
+        line3=findViewById(R.id.line3);
+        line4=findViewById(R.id.line4);
+
+        button=findViewById(R.id.submit_area);
+        Explain=findViewById(R.id.textView2);
+
+
         textView=findViewById(R.id.textview);
    //     option=findViewById(R.id.listOption);
 
-        String work="0000@Name the bill that was passed in the Parliament that seeks to clarify that allottees under a real estate project should be treated as financial creditors.@insolvency and Bankruptcy Code (Second Amendment) Bill, 2018@Recovery of Non-Performing Assets Bill, 2017@Insolvency and Bankruptcy Code (First Amendment) Bill, 2017@Recovery of Non-Performing Assets (Amendment) Bill, 2018";
-        String[] mcq=work.split("@");
+        work="0000@Name the bill that was passed in the Parliament that seeks to clarify that allottees under a real estate project should be treated as financial creditors.@insolvency and Bankruptcy Code (Second Amendment) Bill, 2018@Recovery of Non-Performing Assets Bill, 2017@Insolvency and Bankruptcy Code (First Amendment) Bill, 2017@Recovery of Non-Performing Assets (Amendment) Bill, 2018";
+        mcq=work.split("@");
+        Explainaton="this is the Explainaton";
         List<String> list=new ArrayList<>();
         list.add("A: "+mcq[2]);
         list.add("B: "+mcq[3]);
@@ -53,6 +93,47 @@ public class MainActivity extends AppCompatActivity {
     //    option.setAdapter(adapter);
 
         textView.setText(mcq[1]);
+        opA.setText("A: "+mcq[2]);
+        opB.setText("B: "+mcq[3]);
+        opC.setText("C: "+mcq[4]);
+        opD.setText("D: "+mcq[5]);
+
+        opA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                line2.setVisibility(View.GONE);
+                line3.setVisibility(View.GONE);
+                line4.setVisibility(View.GONE);
+            }
+        });
+        opB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                line1.setVisibility(View.GONE);
+                line3.setVisibility(View.GONE);
+                line4.setVisibility(View.GONE);
+            }
+        });
+        opC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                line2.setVisibility(View.GONE);
+                line1.setVisibility(View.GONE);
+                line4.setVisibility(View.GONE);
+            }
+        });
+        opD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                line2.setVisibility(View.GONE);
+                line3.setVisibility(View.GONE);
+                line1.setVisibility(View.GONE);
+            }
+        });
+
+
+
+
 
 
 //        String url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
@@ -86,5 +167,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doSubmit(View view) {
+        Explain.setText(Explainaton);
+        button.setVisibility(View.INVISIBLE);
     }
 }
